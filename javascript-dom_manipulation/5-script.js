0a4a7df1ddf77925317910b87b1f1d9386c4c08a
JaRenie-spec <renie.jeanalain@gmail.com>
2025-04-02 10:32:06 +0200

    Delete javascript-dom_manipulation directory/ wrong repo

--- javascript-dom_manipulation/5-script.js
+++
@@ -1,5 +0,0 @@
-const updateHeaderButton = document.getElementById('update_header');
-updateHeaderButton.addEventListener('click', function() {
-  const header = document.querySelector('header');
-  header.textContent = 'New Header!!!';
-});
--- javascript-dom_manipulation/8-script.js
+++
@@ -1,9 +0,0 @@
-fetch('https://hellosalut.stefanbohacek.dev/?lang=fr')
-  .then(response => response.json())
-  .then(data => {
-    const translatedHello = data.hello;
-    document.getElementById('hello').textContent = translatedHello;
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
