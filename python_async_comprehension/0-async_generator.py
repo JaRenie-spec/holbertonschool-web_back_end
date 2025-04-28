b8f264b6946d6cada4207082d6ab6dbb20127ea5
JaRenie-spec <renie.jeanalain@gmail.com>
2025-04-28 11:06:08 +0200

    adding typing generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """Module containing an asynchronous generator that yields random numbers."""
+
 import asyncio
 import random
+import typing
 
 
-async def async_generator():
+async def async_generator() -> typing.Generator[float, None, None]:
     """Coroutine that yields a random number between 0 and 10, 10 times,
       with 1 second delay each."""
     for _ in range(10):
