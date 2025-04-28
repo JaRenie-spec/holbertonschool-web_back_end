d520d213ab5067ef84b380ffa498aba1d8488cb4
JaRenie-spec <renie.jeanalain@gmail.com>
2025-04-28 10:59:27 +0200

    fixing column

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Module containing a coroutine that collects random numbers asynchronously."""
+"""Module containing a coroutine that
+collects random numbers asynchronously."""
 
 from typing import List
 async_generator = __import__('0-async_generator').async_generator
